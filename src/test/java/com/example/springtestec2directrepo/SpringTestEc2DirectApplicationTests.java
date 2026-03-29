217ff82a102e3349829e6947ae632637d4bdaca9
faizunroyalbengalai <faizun.royalbengalai@gmail.com>
2026-03-29 20:48:02 +0600

    chore: bootstrap Spring Boot starter from Spring Initializr

---
+++ src/test/java/com/example/springtestec2directrepo/SpringTestEc2DirectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springtestec2directrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestEc2DirectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
